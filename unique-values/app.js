53361602f8bd5e89975ca073c50cdf29b38ae942
kadirermaya <kadir.ermaya@gmail.com>
2021-03-11 00:20:13 -0800

    practiced to get unique values from categories with ...new Set

---
+++ unique-values/app.js
@@ -0,0 +1,41 @@
+// Unique Values
+const menu = [
+  {
+    name: "omelette",
+    category: "breakfast",
+  },
+  {
+    name: "cheese burger",
+    category: "lunch",
+  },
+  {
+    name: "kabob",
+    category: "dinner",
+  },
+  {
+    name: "bacon",
+    category: "breakfast",
+  },
+  {
+    name: "eggs",
+    category: "breakfast",
+  },
+  {
+    name: "cheesecake",
+    category: "dinner",
+  },
+];
+
+const categories = ["all", ...new Set(menu.map((item) => item.category))];
+console.log(categories);
+
+// map - get all instances
+// new Set - narrow down
+// ['all', ...] - turn it back to array
+
+const result = document.querySelector(".result");
+result.innerHTML = categories
+  .map((category) => {
+    return `<button>${category}</button>`;
+  })
+  .join("");
