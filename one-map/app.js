2d05e1fc688f52508ef2bcb6d9f88a36ba47d782
kadirermaya <kadir.ermaya@gmail.com>
2021-03-10 23:45:45 -0800

    names printed into the #result

--- one-map/app.js
+++ one-map/app.js
@@ -21,17 +21,21 @@ const people = [
   },
 ];
 
-const getAges = (person) => person.age * 2;
+const getAges = (person) => person.age;
 
 const ages = people.map(getAges);
 console.log(ages);
 
-const newPeople = people.map((person) => {
+const newPeople = people.map((item) => {
   return {
-    firstname: person.name.toUpperCase(),
-    oldAge: person.age + 20,
-    newPosition: person.position.toLowerCase(),
+    firstname: item.name.toUpperCase(),
+    oldAge: item.age + 20,
+    newPosition: item.position.toLowerCase(),
   };
 });
 
 console.log(newPeople);
+
+const names = people.map((people) => `<h2>${people.name}</h2>`);
+const result = document.querySelector("#result");
+result.innerHTML = names.join("");
