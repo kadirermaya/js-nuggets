7799666c74d85eaffb3d97a5bd176c1ebd74e060
kadirermaya <kadir.ermaya@gmail.com>
2021-03-17 23:59:59 -0700

    reduce basics folder created

---
+++ reduce-basics/app.js
@@ -0,0 +1,22 @@
+const staff = [
+  {
+    name: "kadir",
+    age: 31,
+    position: "designer",
+  },
+  {
+    name: "selcuk",
+    age: 34,
+    position: "lead DEVELOPER",
+  },
+  {
+    name: "ridvan",
+    age: 29,
+    position: "sr DEVELOPER",
+  },
+  {
+    name: "mustafa",
+    age: 26,
+    position: "sr DEVELOPER",
+  },
+];
