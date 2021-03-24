394dcc0d640125993a952b2c5269100ae65a6f1a
kadirermaya <kadir.ermaya@gmail.com>
2021-03-24 00:05:24 -0700

    reference, copy and 2018 - ES8 Object practiced with spread seperator

--- spread-seperator/app.js
+++ spread-seperator/app.js
@@ -7,3 +7,21 @@ const father = "refik";
 const uncles = ["ismail", "recep"];
 const relatives = [...kids, father, ...uncles];
 console.log(relatives);
+
+//reference
+// const newRelatives = relatives;
+// newRelatives[0] = "kaaadir";
+// console.log(newRelatives);
+
+//copy
+const newRelatives = [...relatives];
+newRelatives[0] = "kaaadir";
+console.log(newRelatives);
+console.log(relatives);
+//2018 - ES8 Objects
+
+const person = { name: "murat", job: "scientist" };
+const newPerson = { ...person, city: "chicago", name: "peter" };
+
+console.log(person);
+console.log(newPerson);
