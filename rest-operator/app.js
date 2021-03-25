aba59c773dd136faa968305cea36990576cea35c
Kadir Ermaya <kadir.ermaya@gmail.com>
2021-03-25 00:07:13 -0400

    rest operator practiced

--- rest-operator/app.js
+++ rest-operator/app.js
@@ -10,3 +10,22 @@ const fruits= ['apple', 'orange', 'lemon', 'banana'];
 const [first,...restOfTheFruits] = fruits;
 
 console.log(first,restOfTheFruits);
+const specificFruit = restOfTheFruits.find((fruit) => fruit === 'orange');
+console.log(specificFruit);
+
+//objects
+
+const person = {name: 'kadir', lastName: 'ermaya', job: 'developer'};
+const {job,...rest} = person;
+console.log(job,rest);
+
+//funtions
+
+const getAverage = (name,...scores) =>{
+    console.log(name);
+    console.log(scores);
+    const average = scores.reduce((total,item)=> {return total += item},0)/scores.length;
+    console.log(average);
+}
+const testScores = [23,45,67,89];
+getAverage(person.name,...testScores);
