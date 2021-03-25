7f5f9498ba8a13a60f509e61b195934db356cfb5
Kadir Ermaya <kadir.ermaya@gmail.com>
2021-03-24 23:55:41 -0400

    rest-operator folder created

---
+++ rest-operator/app.js
@@ -0,0 +1,12 @@
+//rest operator
+//gathers/collets itesms
+//destructuring, functions
+//placement important, careful with the same name
+//rest when declare funtion, spread when invoke the function
+
+//arrays
+
+const fruits= ['apple', 'orange', 'lemon', 'banana'];
+const [first,...restOfTheFruits] = fruits;
+
+console.log(first,restOfTheFruits);
